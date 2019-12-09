ecf3c8cd62583709a780ef75125fe7f414c88860
carlosseguracern <carlosseguracern@u.boisestate.edu>
2019-12-09 09:36:54 -0700

    update data file

--- search_engine_code/commander.py
+++ search_engine_code/commander.py
@@ -1,5 +1,5 @@
 from controller import Controller
 
-fileName = "../data/top1k.csv" #"../data/wikipedia_text_files.csv"
+fileName = "../data/wikipedia_text_files.csv"
 controller = Controller()
 controller.build_structure(fileName)
